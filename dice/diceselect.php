758a685a0a64603f2985965c36d5660917ac2092
Pedro Flores <armandorf@users.noreply.github.com>
2015-05-13 17:37:26 -0700

    listed dice operations

--- dice/diceselect.php
+++ dice/diceselect.php
@@ -7,9 +7,12 @@
   <body>
 	<h2>Select a Dice Operation:</h2>
     <a href="">Product Categories</a><br>
-	<a href="">...</a><br>
-	<a href="">...</a><br>
-	<a href="">...</a><br><br>
+	<a href="dice_prod_by_brands.php">Dice by Brands: Big Can or Cold Gourmet</a><br>
+	<a href="dice_prod_by_category.php">Dice by Categories: Drinks or Food</a><br>
+	<a href="dice_time_by_week_of_year.php">Dice by Week of Year: Week 40 or 42</a><br>
+	<a href="dice_time_by_weekdays.php">Dice by Days of Week: Weekdays</a><br>
+	<a href="dice_time_by_weekend.php">Dice by Days of Week: Weekends</a><br><br>
+	
 	<a href="../index.php">Home</a><br>
   </body>
 </html>
